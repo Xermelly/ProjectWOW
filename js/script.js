c656b199da42b5d69c26ce68051cb2101ab37666
Christopher C. Santos <closedman_@hotmail.com>
2022-10-15 18:06:51 -0300

    JS Effect on member-list

--- js/script.js
+++ js/script.js
@@ -4,25 +4,60 @@ console.log(memberContainer);
 
 function onMouseOver(item) {
   const memberHover = item.getAttribute("id");
-  let statsList;
+
   function showAtt(event) {
     switch (memberHover) {
       case "tank":
         statsList = document.querySelector("#healer");
         statsList.classList.toggle("active");
-        console.log("Sou Tank");
+        if (event.type === "mouseover") {
+          statsList.style.backgroundImage = "url(../../img/fullimg/monk2.png)";
+        }
+        if (event.type === "mouseout") {
+          statsList.style.backgroundImage = "";
+        }
         break;
       case "healer":
-        console.log("Sou Healer");
+        statsList = document.querySelector("#dps-1");
+        statsList.classList.toggle("active");
+        if (event.type === "mouseover") {
+          statsList.style.backgroundImage =
+            "url(../../img/fullimg/healer2.jpg)";
+        }
+        if (event.type === "mouseout") {
+          statsList.style.backgroundImage = "";
+        }
         break;
       case "dps-1":
-        console.log("Sou Walock");
+        statsList = document.querySelector("#dps-2");
+        statsList.classList.toggle("active");
+        if (event.type === "mouseover") {
+          statsList.style.backgroundImage =
+            "url(../../img/fullimg/warlock2.png)";
+        }
+        if (event.type === "mouseout") {
+          statsList.style.backgroundImage = "";
+        }
         break;
       case "dps-2":
-        console.log("Sou Rogue");
+        statsList = document.querySelector("#dps-3");
+        statsList.classList.toggle("active");
+        if (event.type === "mouseover") {
+          statsList.style.backgroundImage = "url(../../img/fullimg/rogue2.png)";
+        }
+        if (event.type === "mouseout") {
+          statsList.style.backgroundImage = "";
+        }
         break;
       case "dps-3":
-        console.log("Sou Mage");
+        statsList = document.querySelector("#dps-2");
+        statsList.classList.toggle("active");
+        if (event.type === "mouseover") {
+          statsList.style.backgroundImage = "url(../../img/fullimg/mage2.png)";
+        }
+        if (event.type === "mouseout") {
+          statsList.style.backgroundImage = "";
+        }
         break;
     }
   }
